34dbcd539ef7d3b7609c28c5239dbb7dcbcd2f75
steel574 <fatima.stahl@live.se>
2021-05-17 11:27:40 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,9 +1,23 @@
 <!DOCTYPE HTML>
 <HTML>
     <HEAD>
-    <LINK REL="stylesheet" TYPE="text/css" HREF="./css/style.css">
     <META CHARSET="UTF-8">
     </HEAD>
     <BODY>
+    <div id="wrapper">
+    <form name="myform" method="post" action="" onsubmit="">
+    <label for="name"></label>
+    <input type="text" id="name" name="name" placeholder="Namn">
+    <label for="email"></label>
+    <input type="text" id="email" name="email" placeholder="E-postadress">
+    <label for="password"></label>
+    <input type="text" id="password" name="password" placeholder="Lösenord">
+    <label for="reppassword"></label>
+    <input type="text" id="email" name="reppassword" placeholder="Repetera lösenord">
+    <br>
+    <br>
+    <input type="submit" value="Registrera dig" id="send">
+    </form>
+    </div>
     </BODY>
 </HTML>
