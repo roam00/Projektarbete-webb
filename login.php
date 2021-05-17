1ebf503f115f15739c31276b1a91e913183d7e6e
wictormagnusson <wictor.magnusson@outlook.com>
2021-05-17 11:16:09 +0200

    Update login.php

--- login.php
+++ login.php
@@ -5,5 +5,12 @@
     <META CHARSET="UTF-8">
     </HEAD>
     <BODY>
+    <form>
+  <label for="uname">Användarnamn:</label>
+  <input type="text" id="uname" name="uname"><br><br>
+  <label for="pword">Lösenord:</label>
+  <input type="text" id="pword" name="pword"><br><br>
+  <input type="submit" value="Logga in">
+</form>
     </BODY>
 </HTML>
