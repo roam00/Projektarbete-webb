85e723c300e414994755e81b5fdf2642b1b58523
wictormagnusson <wictor.magnusson@outlook.com>
2021-05-18 13:24:58 +0200

    Startsida

---
+++ homepage.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<HTML>
+    <HEAD>
+    <LINK REL="stylesheet" TYPE="text/css" HREF="./css/style.css">
+    <META CHARSET="UTF-8">
+    </HEAD>
+    <BODY>
+    <?PHP
+
+    ?>
+    </BODY>
+</HTML>
--- login.php
+++ login.php
@@ -5,12 +5,12 @@
     <META CHARSET="UTF-8">
     </HEAD>
     <BODY>
-    <form>
-  <label for="uname">Användarnamn:</label>
-  <input type="text" id="username" name="username"><br><br>
-  <label for="password">Lösenord:</label>
-  <input type="text" id="password" name="password"><br><br>
-  <input type="submit" value="Logga in">
+<form action="homepage.php">
+      <label for="uname">Användarnamn:</label>
+      <input type="text" id="username" name="username"><br><br>
+      <label for="password">Lösenord:</label>
+      <input type="text" id="password" name="password"><br><br>
+     <input type="submit" value="Logga in">
 </form>
     </BODY>
 </HTML>
