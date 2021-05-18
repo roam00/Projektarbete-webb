8c226e6c3adacdfed1ccf8faca8a05a8506e265f
nothxor <nothxor@gmail.com>
2021-05-18 14:45:07 +0200

    Update cocktail.php

--- cocktail.php
+++ cocktail.php
@@ -33,35 +33,66 @@
     echo '<BR>';
     $imglink = $arr3['strDrinkThumb'];
     echo '<BR>';
+    if ($arr3['strIngredient1']) {
     echo $arr3['strIngredient1'] . ' - ' . $arr3['strMeasure1'];
     echo '<BR>';
+    }
+    if ($arr3['strIngredient2']) {
     echo $arr3['strIngredient2'] . ' - ' . $arr3['strMeasure2'];
     echo '<BR>';
+    }
+    if ($arr3['strIngredient3']) {
     echo $arr3['strIngredient3'] . ' - ' . $arr3['strMeasure3'];
     echo '<BR>';
+    }
+    if ($arr3['strIngredient4']) {
     echo $arr3['strIngredient4'] . ' - ' . $arr3['strMeasure4'];
     echo '<BR>';
+    }
+    if ($arr3['strIngredient5']) {
     echo $arr3['strIngredient5'] . ' - ' . $arr3['strMeasure5'];
     echo '<BR>';
+    }
+    if ($arr3['strIngredient6']) {
     echo $arr3['strIngredient6'] . ' - ' . $arr3['strMeasure6'];
     echo '<BR>';
+    }
+    if ($arr3['strIngredient7']) {
     echo $arr3['strIngredient7'] . ' - ' . $arr3['strMeasure7'];
     echo '<BR>';
+    }
+    if ($arr3['strIngredient8']) {
     echo $arr3['strIngredient8'] . ' - ' . $arr3['strMeasure8'];
     echo '<BR>';
+    }
+    if ($arr3['strIngredient9']) {
     echo $arr3['strIngredient9'] . ' - ' . $arr3['strMeasure9'];
     echo '<BR>';
+    }
+    if ($arr3['strIngredient10']) {
     echo $arr3['strIngredient10'] . ' - ' . $arr3['strMeasure10'];
     echo '<BR>';
+    }
+    if ($arr3['strIngredient11']) {
     echo $arr3['strIngredient11'] . ' - ' . $arr3['strMeasure11'];
     echo '<BR>';
+    }
+    if ($arr3['strIngredient12']) {
     echo $arr3['strIngredient12'] . ' - ' . $arr3['strMeasure12'];
     echo '<BR>';
+    }
+    if ($arr3['strIngredient13']) {
     echo $arr3['strIngredient13'] . ' - ' . $arr3['strMeasure13'];
     echo '<BR>';
+    }
+    if ($arr3['strIngredient14']) {
     echo $arr3['strIngredient14'] . ' - ' . $arr3['strMeasure14'];
     echo '<BR>';
+    }
+    if ($arr3['strIngredient15']) {
     echo $arr3['strIngredient15'] . ' - ' . $arr3['strMeasure15'];
+    echo '<BR>';
+    }
     echo '<BR>';
 ?>
 
