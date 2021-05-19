9bbe62a4b0613c1975c0ec6076d89ac76ee56090
nothxor <nothxor@gmail.com>
2021-05-19 10:32:03 +0200

    Update bored_results.php

--- bored_results.php
+++ bored_results.php
@@ -23,9 +23,17 @@
     }
     $result = file_get_contents($finalurl);
     $details = json_decode($result, TRUE);
-    var_dump($details);
     echo '<BR>';
-    echo $details['activity'];
+    echo 'activity: ' . $details['activity'];
+    echo '<BR>';
+    echo 'type: ' . $details['type'];
+    echo '<BR>';
+    echo 'participants: ' . $details['participants'];
+    echo '<BR>';
+    echo 'price: ' . $details['price'];
+    echo '<BR>';
+    echo 'accessibility: ' . $details['accessibility'];
+
     ?>
     </BODY>
 </HTML>
