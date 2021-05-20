9e4020bed640d39992130c8d39323714b06b2c54
nothxor <nothxor@gmail.com>
2021-05-20 11:37:56 +0200

    210520-02

--- randomize_activity.php
+++ randomize_activity.php
@@ -31,36 +31,6 @@
                 <OPTION VALUE="4">4</OPTION>
             </SELECT>
             <BR>
-            <LABEL FOR="maxprice">Max cost:</LABEL>
-            <SELECT ID="maxprice" NAME="maxprice">
-                <OPTION VALUE="">-</OPTION>
-                <OPTION VALUE="0.1">0.1</OPTION>
-                <OPTION VALUE="0.2">0.2</OPTION>
-                <OPTION VALUE="0.3">0.3</OPTION>
-                <OPTION VALUE="0.4">0.4</OPTION>
-                <OPTION VALUE="0.5">0.5</OPTION>
-                <OPTION VALUE="0.6">0.6</OPTION>
-                <OPTION VALUE="0.7">0.7</OPTION>
-                <OPTION VALUE="0.8">0.8</OPTION>
-                <OPTION VALUE="0.9">0.9</OPTION>
-                <OPTION VALUE="1">1</OPTION>
-            </SELECT>
-            <BR>
-            <LABEL FOR="maxaccessibility">Accessibility (lower number is more accessible)</LABEL>
-            <SELECT ID="maxaccessibility" NAME="maxaccessibility">
-                <OPTION VALUE="">-</OPTION>
-                <OPTION VALUE="0.0">0.0</OPTION>
-                <OPTION VALUE="0.1">0.1</OPTION>
-                <OPTION VALUE="0.2">0.2</OPTION>
-                <OPTION VALUE="0.3">0.3</OPTION>
-                <OPTION VALUE="0.4">0.4</OPTION>
-                <OPTION VALUE="0.5">0.5</OPTION>
-                <OPTION VALUE="0.6">0.6</OPTION>
-                <OPTION VALUE="0.7">0.7</OPTION>
-                <OPTION VALUE="0.8">0.8</OPTION>
-                <OPTION VALUE="0.9">0.9</OPTION>
-                <OPTION VALUE="1.0">1.0</OPTION>
-            <BR>
             <INPUT TYPE="submit" VALUE="Skicka!">
             </FIELDSET>
         </FORM>            
