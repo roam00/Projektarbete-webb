eb3a2befba7d007353bdf5f80c1438a02520f7d7
roam00 <63878881+roam00@users.noreply.github.com>
2021-05-20 12:26:47 +0200

    Randomzier

--- randomizer.php
+++ randomizer.php
@@ -12,8 +12,23 @@
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
+    <div class="box" id="div1">
 <?php getCocktail(); ?>
-<BR><BR>
+</div>
+<div class="box" id="div2">
 <?php getBored(); ?>
+</div>
+<br><br>
+
+<button onClick="window.location.reload();">Randomize again</button>
+
+<div class="box" id="header">
+    <h1> Save this combination </h1>
+    <form action="./db/dbCombination.php">
+    <button type="submit"> Save the combination</button>
+    </form>
+</div>
+
 </body>
+
 </html>
