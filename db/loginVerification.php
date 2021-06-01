6d65b78eec93505c65c0c27036a64246614d0876
roam00 <63878881+roam00@users.noreply.github.com>
2021-06-01 14:07:38 +0200

    gh

--- db/loginVerification.php
+++ db/loginVerification.php
@@ -3,11 +3,12 @@
 include "dbUser.php";
 session_start();
 
-$_SESSION['userType'] = FindUserTypeByUserId($_POST['username']);
 
 $_SESSION['username'] = $_POST['username'];
 
 $_SESSION['userID'] = FindIdByUsername($_POST['username']);
+
+$_SESSION['userType'] = FindUserTypeByUserId($_SESSION['userID']);
 
 $_SESSION['email'] = FindEmailByUserId(FindIdByUsername($_POST['username']));
 
