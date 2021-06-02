7167f07899ab0d9fd859d1d7dd90b04b2b40c4ca
wictormagnusson <wictor.magnusson@outlook.com>
2021-06-02 21:19:03 +0200

    css

--- HTML/header.php
+++ HTML/header.php
@@ -5,11 +5,12 @@
         if(isset($_SESSION['username'])){
             echo "<li style='float: right;'><a href='profile.php'>My profile</a></li>
             <li style='float: right;'><a href='logout.php'>Log out</a></li>";
-            echo "<li><a href='homepage.php'>Home</a></li>";
+            echo "<form action = 'homepage.php'><button class = 'back_button'>←</button></form>";
         }
 
         else {
-            echo "<li><a href='index.php'>Back</a></li>";
+            echo "<form action = 'index.php'><button class = 'back_button'>←</button></form>";
+            
         }
     ?>
 </ul>
--- profile.php
+++ profile.php
@@ -1,44 +1,39 @@
-<?php session_start(); ?>
+<?php 
+session_start(); 
+if(!isset($_SESSION['username'])){
+    header('Location: index.php');
+}
+?>
 <!DOCTYPE html>
 <html>
-
 <head>
     <title> Home </title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="./css/style.css">
 </head>
+    <body>
+        <?php
+            include "HTML/header.php";
+        ?>
 
-<body>
+        <h1> Hi <?php echo $_SESSION['username']; ?> </h1>
+        <h1> Usertype: <?php echo $_SESSION['userType']; ?> </h1>
 
-    <?php
-include "HTML/header.php";
-?>
-
-    <h1> Hi <?php echo $_SESSION['username']; ?> </h1>
-    <h1> Usertype: <?php echo $_SESSION['userType']; ?> </h1>
-
-    <?php
-
-    if ($_SESSION['userType'] == 1) {
-        echo '<FORM ID="addnews" NAME="addnews" METHOD="post" ACTION="addnews.php">
-        <FIELDSET>
-        <LEGEND>Add news:</LEGEND>
-        <TEXTAREA NAME="comment" ID="comment" FORM="addnews" ROWS="4" COLS="50"></TEXTAREA>
-        <BR>
-        <INPUT TYPE="submit" VALUE="Submit">
-        </FIELDSET>
-        </FORM>
-
-        ';
-    }
-
-?>
-    
+        <?php
+            if ($_SESSION['userType'] == 1) {
+                echo '<FORM ID="addnews" NAME="addnews" METHOD="post" ACTION="addnews.php">
+                <FIELDSET>
+                <LEGEND>Add news:</LEGEND>
+                <TEXTAREA NAME="comment" ID="comment" FORM="addnews" ROWS="4" COLS="50"></TEXTAREA>
+                <BR>
+                <INPUT TYPE="submit" VALUE="Submit">
+                </FIELDSET>
+                </FORM>';
+            }
+        ?>
 
     <?php
-include "HTML/footer.php";
-?>
-
-</body>
-
+        include "HTML/footer.php";
+    ?>
+    </body>
 </html>
