06b2d3fc1f2a33b37b357c05b1b5639799e46d9d
nothxor <nothxor@gmail.com>
2021-06-02 10:08:29 +0200

    210602-1008

--- search.php
+++ search.php
@@ -31,6 +31,26 @@ include "HTML/header.php";
             <INPUT TYPE="submit" VALUE="Send!">
         </FIELDSET>
     </FORM>
+    <BR><BR>
+    <FORM NAME="cocktail" METHOD="post" ACTION="searchcocktail.php">
+        <FIELDSET>
+            <LEGEND>Search cocktails:</LEGEND>
+            <LABEL FOR="type">Activity type:</LABEL>
+            <SELECT ID="type" NAME="type">
+                <OPTION VALUE="">-</OPTION>
+                <OPTION VALUE="education">Education</OPTION>
+                <OPTION VALUE="recreational">Recreational</OPTION>
+                <OPTION VALUE="social">Social</OPTION>
+                <OPTION VALUE="diy">DIY</OPTION>
+                <OPTION VALUE="charity">Charity</OPTION>
+                <OPTION VALUE="cooking">Cooking</OPTION>
+                <OPTION VALUE="relaxation">Relaxation</OPTION>
+                <OPTION VALUE="music">Music</OPTION>
+                <OPTION VALUE="busywork">Busywork</OPTION>
+            </SELECT>
+            <INPUT TYPE="submit" VALUE="Send!">
+        </FIELDSET>
+    </FORM>
 
 
 <?php
