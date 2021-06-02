863360246a8d6b007b6fed3fc991328bf8645743
wictormagnusson <wictor.magnusson@outlook.com>
2021-06-02 10:10:41 +0200

    css

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html>
 
 <head>
-<title> Welcome page </title>
+    <title> Welcome page </title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="./css/style.css">
 </head>
@@ -13,24 +13,24 @@
         include "HTML/header.php";
     ?>
 
-<div class = "title_box"> 
-    <text><br>aMatch</text>  
-</div>
+    <div class = "title_box"> 
+        <text><br>aMatch</text>  
+    </div>
 
-<div class = "button_box">
-    <form action = 'login.php'>
-        <button class= 'buttons' type = 'submit'>Log in</button>
-    </form>
-        <div class = 'divider'></div>
-    <form action = 'register.php'>
-        <button class= 'buttons' type = 'submit'>Register</button>
-    </form>
-</div>
+    <div class = "button_box">
+        <form action = 'login.php'>
+            <button class= 'buttons' type = 'submit'>Log in</button>
+        </form>
+            <div class = 'divider'></div>
+        <form action = 'register.php'>
+            <button class= 'buttons' type = 'submit'>Register</button>
+        </form>
+    </div>
 
-<?php
-include "HTML/footer.php";
-?>
+    <?php
+        include "HTML/footer.php";
+    ?>
 
-</body>
+</body> 
 
 </html>
