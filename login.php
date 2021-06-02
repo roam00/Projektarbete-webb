558cc91b990a0e8ffa91b5ca5045ce84660ebd98
wictormagnusson <wictor.magnusson@outlook.com>
2021-06-02 08:50:08 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,22 +1,24 @@
 <!DOCTYPE HTML>
 <HTML>
     <HEAD>
-    <LINK REL="stylesheet" TYPE="text/css" HREF="./css/style.css">
-    <META CHARSET="UTF-8">
+        <LINK REL="stylesheet" TYPE="text/css" HREF="./css/style.css">
+        <META CHARSET="UTF-8">
     </HEAD>
     <BODY>
-
+        <?php
+            include "HTML/header.php";
+        ?>
     <!-- används för att skriva ut errormdeelaenaden från javascript-->
-    <div id="error"></div>
-    <div class = "log_in_box">
-        <form action="./db/loginverification.php" onsubmit="return validate_login()" method="post" id="formId">
-            <label for="uname"></label>
-            <input class = 'input' type="text" id="username" name="username" placeholder="Username"><br><br>
-            <label for="password"></label>
-            <input class = 'input' type="password" id="password" name="password" placeholder="Password"><br><br>
-            <button class= 'button' type = 'submit'>Log in</button>
-        </form>
-    </div>
-<script src="./js/script.js"></script>
+        <div id="error"></div>
+        <div class = "log_in_box">
+            <form action="./db/loginverification.php" onsubmit="return validate_login()" method="post" id="formId">
+                <label for="uname"></label>
+                <input class = 'input' type="text" id="username" name="username" placeholder="Username"><br><br>
+                <label for="password"></label>
+                <input class = 'input' type="password" id="password" name="password" placeholder="Password"><br><br>
+                <button class= 'button' type = 'submit'>Log in</button>
+            </form>
+        </div>
+        <script src="./js/script.js"></script>
     </BODY>
 </HTML>
