d03eca94ffedf96bae6f95b3025994045231b2d5
wictormagnusson <wictor.magnusson@outlook.com>
2021-06-02 15:08:00 +0200

    css

--- HTML/header.php
+++ HTML/header.php
@@ -5,7 +5,7 @@
         if(isset($_SESSION['username'])){
             echo "<li style='float: right;'><a href='profile.php'>My profile</a></li>
             <li style='float: right;'><a href='logout.php'>Log out</a></li>";
-            echo "<li><a href='index.php'>Home</a></li>";
+            echo "<li><a href='homepage.php'>Home</a></li>";
         }
 
         else {
