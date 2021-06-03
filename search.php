cd194ed7158528889471248865efa0ca25db4d16
wictormagnusson <wictor.magnusson@outlook.com>
2021-06-03 09:43:27 +0200

    css

--- search.php
+++ search.php
@@ -28,7 +28,7 @@ include "HTML/header.php";
                 <OPTION VALUE="music">Music</OPTION>
                 <OPTION VALUE="busywork">Busywork</OPTION>
             </SELECT>
-            <INPUT TYPE="submit" VALUE="Send!">
+            <button class = 'button8' TYPE="submit">Search</button>
         </FIELDSET>
     </FORM>
     <BR><BR>
@@ -48,7 +48,7 @@ include "HTML/header.php";
                 <OPTION VALUE="Shot">Shot</OPTION>
                 <OPTION VALUE="Soft Drink / Soda">Soft Drink / Soda</OPTION>
             </SELECT>
-            <button class = 'button8' TYPE="submit" VALUE="Send!">
+            <button class = 'button9' TYPE="submit">Search</button>
         </FIELDSET>
     </FORM>
 </div>
