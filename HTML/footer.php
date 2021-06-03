3a20aad73b272e2118f5c18aaa7597780cb28f44
wictormagnusson <wictor.magnusson@outlook.com>
2021-06-03 11:38:40 +0200

    Update footer.php

--- HTML/footer.php
+++ HTML/footer.php
@@ -1,6 +1,6 @@
 <div class="footer">
         <div class = 'copyright'>
-                Copyright © 2021 by Stefan Sandberg, Fatima Ståhl, Hampus Backe, Wictor Magnusson All Rights Reserved
+                Copyright © 2021 by Stefan Sandberg, Fatima Ståhl, Hampus Back, Wictor Magnusson All Rights Reserved
         </div>
         <ul>
                 <li><a href="adminpage.php">Admin</a></li>
